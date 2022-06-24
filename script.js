44dc8aaa812d18e5fae0cc618b586f5bf59b9c54
bimpe030 <adelanwa.zainab05@gmail.com>
2022-06-25 00:21:17 +0100

    fixed the calculator

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-function clearScreen() {
-    document.getElementById("result").value = "";
+function calculator(){
+    let calculate = prompt("What action would you like to perform (Addition, Sutraction, Multiplication or Division)");
+
+    let num1 = prompt ("Enter First Number:");
+    let num2 = prompt ("Enter Second Number");
+
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+
+    if (calculate == "addition"){
+        alert(num1+num2)
+    }
+
+    else if (calculate == "subtraction"){
+        alert(num1-num2)
+    }
+
+    else if (calculate == "division"){
+        alert(num1/num2)
+    }
+
+    else if (calculate == "multiplication"){
+        alert(num1*num2)
+    }
+
+    else{
+        alert("Invalid Selection")
+    }
+
+    let anotherOperation = prompt("Would you like to perform another calculation");
+
+    if (anotherOperation == "yes"){
+        calculator()
+    }  else {
+        alert("Thank you, Bye Bye")
+    }
 }
 
-function display(value) {
-    document.getElementById("result").value += value;
-}
 
-function calculate() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
-}
+calculator ()
