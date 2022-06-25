36ec1640d74dd919ec8fbd5f1363bf6f102e6b46
bimpe030 <adelanwa.zainab05@gmail.com>
2022-06-25 20:06:31 +0100

    fixed an issue

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
 function calculator(){
     let calculate = prompt("What action would you like to perform (Addition, Sutraction, Multiplication or Division)");
 
-    let num1 = prompt ("Enter First Number:");
-    let num2 = prompt ("Enter Second Number");
-
-    num1 = parseFloat(num1)
-    num2 = parseFloat(num2)
 
     if (calculate == "addition"){
+        let num1 = prompt ("Enter First Number:");
+        let num2 = prompt ("Enter Second Number");
+
+        num1 = parseFloat(num1)
+        num2 = parseFloat(num2)
+
         alert(num1+num2)
     }
 
     else if (calculate == "subtraction"){
+        let num1 = prompt ("Enter First Number:");
+        let num2 = prompt ("Enter Second Number");
+
+        num1 = parseFloat(num1)
+        num2 = parseFloat(num2)
+
         alert(num1-num2)
     }
 
     else if (calculate == "division"){
+        let num1 = prompt ("Enter First Number:");
+        let num2 = prompt ("Enter Second Number");
+
+        num1 = parseFloat(num1)
+        num2 = parseFloat(num2)
+
         alert(num1/num2)
     }
 
     else if (calculate == "multiplication"){
+        let num1 = prompt ("Enter First Number:");
+        let num2 = prompt ("Enter Second Number");
+
+        num1 = parseFloat(num1)
+        num2 = parseFloat(num2)
+
         alert(num1*num2)
     }
 
     else{
-        alert("Invalid Selection")
+        alert("Invalid Operation Selected")
     }
 
     let anotherOperation = prompt("Would you like to perform another calculation");
